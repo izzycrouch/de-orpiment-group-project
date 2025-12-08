18704b2d9333c319cb6529df9b018cc9055b548d
izzycrouch <belle.crouch00@gmail.com>
2025-12-08 16:55:29 +0000

    Change s3 bucket resource name.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket" "raw_data_bucket" {
-  bucket_prefix = var.raw_data_bucket_name
+  bucket = var.raw_data_bucket_name
   tags = {
     Name = "raw_bucket"
   }
 }
 
-resource "aws_s3_bucket" "raw_data_bucket" {
-  bucket_prefix = var.dates_bucket_name
+resource "aws_s3_bucket" "date_bucket" {
+  bucket = var.dates_bucket_name
   tags = {
     Name = "dates_bucket"
   }
