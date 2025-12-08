d1329a02801c44430d0d6a34ca0ad7db45c60dfc
Shyqwq5 <shyqwq5@gmail.com>
2025-12-08 15:41:04 +0000

    add tamplate of iam

--- src/utils/get_secret.py
+++ src/utils/get_secret.py
@@ -5,7 +5,6 @@ def get_secret():
     secret_name = "project-orpiment-raw-database"
     region_name = "eu-west-2"
 
-    # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
@@ -17,8 +16,6 @@ def get_secret():
             SecretId=secret_name
         )
     except ClientError as e:
-        # For a list of exceptions thrown, see
-        # <https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html>
         raise e
 
     secret = get_secret_value_response['SecretString']
---
+++ terraform/iam.tf
@@ -0,0 +1,45 @@
+# creat role
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "s3_role" {
+  name               = "s3_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+# data_document
+data "aws_iam_policy_document" "s3_write" {
+  statement {
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+
+#create policy
+resource "aws_iam_policy" "s3_write_policy" {
+  name   = "write_s3_bucket"
+  policy = data.aws_iam_policy_document.s3_write.json
+}
+
+
+#link policy with role
+resource "aws_iam_role_policy_attachment" "attach_read" {
+  role       = aws_iam_role.s3_role.name
+  policy_arn = aws_iam_policy.s3_write_policy.arn
+}
+
