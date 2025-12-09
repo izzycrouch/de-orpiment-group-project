e82bc67e9382f1cb6148bd33eac8ed390d023434
izzycrouch <belle.crouch00@gmail.com>
2025-12-09 11:04:24 +0000

    Fix: change s3_bucket variable to a string.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "extract_raw_data_function" {
     function_name = "extract-func"
     role = aws_iam_role.s3_role.arn
     
-    s3_bucket = aws_s3_bucket.lambda_code_bucket
+    s3_bucket = "${aws_s3_bucket.lambda_code_bucket}"
     s3_key = "extract.zip"
 
     handler = "extract.lambda_handler"
