e5a4484e7317bd9c3d1b2110772b093c2cee32e7
Adina Keller <adinakeller15@gmail.com>
2025-12-10 09:50:22 +0000

    create bucket for terraform remote state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "remote-state-aci"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 
