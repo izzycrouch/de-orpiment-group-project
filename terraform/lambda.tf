1936bc9f94994da9641037e50fc4545eafc2d130
Adina Keller <adinakeller15@gmail.com>
2025-12-10 13:07:23 +0000

    feat: make folders for dependencies

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,27 @@
 resource "aws_lambda_function" "extract_raw_data_function" {
     function_name = "extract-func"
     role = aws_iam_role.s3_role.arn
-    
+
     s3_bucket = "${var.lambda_code_bucket_name}"
     s3_key = "extract.zip"
 
     handler = "extract.lambda_handler"
     runtime = "python3.12"
 }
+
+data "archive_file" "small_layers" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_dir       = "${path.module}/../layer_small/"
+  output_path      = "${path.module}/../layer_small.zip"
+}
+
+data "archive_file" "large_layers" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_dir       = "${path.module}/../layer_large/"
+  output_path      = "${path.module}/../layer_large.zip"
+}
+
+
+
