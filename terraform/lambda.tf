306cd5d3f3d06ffa9f5f4c86b904e418ec8bb20d
Adina Keller <adinakeller15@gmail.com>
2025-12-11 14:47:04 +0000

    feat: lambda func resource for lambda zip function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,20 +1,32 @@
 resource "aws_lambda_function" "extract_raw_data_function" {
-    function_name   = "extract-func"
-    role            = aws_iam_role.s3_role.arn
-    
-    s3_bucket   = "${var.lambda_code_bucket_name}"
-    s3_key      = "extract.zip"
+  function_name   = "extract-func"
+  role            = aws_iam_role.s3_role.arn
+  
+  s3_bucket   = "${var.lambda_code_bucket_name}"
+  s3_key      = "extract.zip"
 
-    handler     = "extract.lambda_handler"
-    runtime     = var.python_runtime
+  handler     = "extract_lambda.lambda_handler"
+  runtime     = var.python_runtime
 
-    layers      = [aws_lambda_layer_version.get_database_layer.arn,
-                   aws_lambda_layer_version.libraries_layer.arn,
-                   aws_lambda_layer_version.utils_layer.arn]
+  layers      = [
+    aws_lambda_layer_version.get_database_layer.arn,
+    aws_lambda_layer_version.libraries_layer.arn,
+    aws_lambda_layer_version.utils_layer.arn
+    ]
 }
 
+resource "aws_lambda_function" "zip_lambda_function" {
+  function_name   = "zip-lambda-func"
+  role            = aws_iam_role.s3_role.arn
+  
+  s3_bucket   = "${var.lambda_code_bucket_name}"
+  s3_key      = "zip_lambda.zip"
 
+  handler     = "zip_lambda_func.lambda_handler"
+  runtime     = var.python_runtime
 
+  layers      = [aws_lambda_layer_version.libraries_layer.arn]
+}
 
 resource "aws_lambda_layer_version" "libraries_layer" {
   s3_bucket     = var.libraries_layer_bucket_name
