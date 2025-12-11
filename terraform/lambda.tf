03cee0f671b83937e064c5561cf584234adb2145
Shyqwq5 <shyqwq5@gmail.com>
2025-12-11 15:39:33 +0000

    add environment variables and timeout for extract lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,44 +1,51 @@
 resource "aws_lambda_function" "extract_raw_data_function" {
-    function_name   = "extract-func"
-    role            = aws_iam_role.s3_role.arn
-    
-    s3_bucket   = "${var.lambda_code_bucket_name}"
-    s3_key      = "extract.zip"
+  function_name = "extract-func"
+  role          = aws_iam_role.s3_role.arn
 
-    handler     = "extract.lambda_handler"
-    runtime     = var.python_runtime
+  s3_bucket = var.lambda_code_bucket_name
+  s3_key    = "extract.zip"
 
-    layers      = [aws_lambda_layer_version.get_database_layer.arn,
-                   aws_lambda_layer_version.libraries_layer.arn,
-                   aws_lambda_layer_version.utils_layer.arn]
+  handler = "extract.lambda_handler"
+  runtime = var.python_runtime
+
+  layers = [aws_lambda_layer_version.get_database_layer.arn,
+    aws_lambda_layer_version.libraries_layer.arn,
+  aws_lambda_layer_version.utils_layer.arn]
+
+  environment {
+    variables = {
+      S3_BUCKET_NAME = aws_s3_bucket.raw_data_bucket.bucket
+    }
+  }
+
+  timeout = 200
 }
-
 
 
 
 resource "aws_lambda_layer_version" "libraries_layer" {
-  s3_bucket     = var.libraries_layer_bucket_name
-  s3_key        = "libraries.zip"
+  s3_bucket = var.libraries_layer_bucket_name
+  s3_key    = "libraries.zip"
 
-  layer_name    = "libraries_layer"
+  layer_name = "libraries_layer"
 
-  compatible_runtimes   = [var.python_runtime]
+  compatible_runtimes = [var.python_runtime]
 }
 
 resource "aws_lambda_layer_version" "utils_layer" {
-  s3_bucket     = var.lambda_code_bucket_name
-  s3_key        = "extract_layer/utils.zip"
+  s3_bucket = var.lambda_code_bucket_name
+  s3_key    = "extract_layer/utils.zip"
 
-  layer_name    = "utils_layer"
+  layer_name = "utils_layer"
 
-  compatible_runtimes   = [var.python_runtime]
+  compatible_runtimes = [var.python_runtime]
 }
 
 resource "aws_lambda_layer_version" "get_database_layer" {
-  s3_bucket     = var.lambda_code_bucket_name
-  s3_key        = "extract_layer/get_database.zip"
+  s3_bucket = var.lambda_code_bucket_name
+  s3_key    = "extract_layer/get_database.zip"
 
-  layer_name    = "get_database_layer"
+  layer_name = "get_database_layer"
 
-  compatible_runtimes   = [var.python_runtime]
+  compatible_runtimes = [var.python_runtime]
 }
