0fa76826e7e650bac96f7a2604e99034a2a2c238
Shyqwq5 <shyqwq5@gmail.com>
2025-12-12 12:11:41 +0000

    feat: modify terraform s3 and lambda.tf
    docs: ignore .zip in .gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+from extract_layer.utils.zip_layer_files import create_zip_layer,upload_zip_to_s3
+
+
+res_l = create_zip_layer('large_layers')
+res_s = create_zip_layer('small_layers')
+res_u = create_zip_layer('extract_layer')
+
+upload_zip_to_s3(res_l,'libraries-layer-aci',"large_libraries.zip")
+upload_zip_to_s3(res_s,'libraries-layer-aci',"small_libraries.zip")
+upload_zip_to_s3(res_u,"lambda-func-code-aci","extract_layer/utils.zip")
+
+from extract_layer.utils.save_data import save_data
