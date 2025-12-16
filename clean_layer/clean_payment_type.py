95ba7048ba47b5d13d73eb92a66f1abfdcb04f8d
Shyqwq5 <shyqwq5@gmail.com>
2025-12-16 16:22:12 +0000

    feat: fix clean payment colomn name error

--- clean_layer/clean_payment_type.py
+++ clean_layer/clean_payment_type.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from clean_layer.utils.get_raw_data import get_df
+from clean_layer.utils.get_df import get_df
 
 def clean_payment_type(file_path: str, bucket_name: str = 'totesys-raw-data-aci'):
     df = get_df(bucket_name=bucket_name, file_name=file_path)
@@ -9,7 +9,7 @@ def clean_payment_type(file_path: str, bucket_name: str = 'totesys-raw-data-aci'
     df['last_updated'] = pd.to_datetime(df['last_updated'], errors='coerce')
     df["payment_type_name"] = df["payment_type_name"].astype("string")
     df = df.dropna()
-    df = df.drop_duplicates(subset=['sales_order_id'], keep='first')
+    df = df.drop_duplicates(subset=['payment_type_id'], keep='first')
     now = pd.Timestamp.now()
     df = df[(df['created_at'] <= now) & (df['last_updated'] <= now) & (df['created_at'] <= df['last_updated'])]
     return df
