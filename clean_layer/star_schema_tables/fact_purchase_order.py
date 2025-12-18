e947c78cca47bc97ac22ee94ce7dbfb5739cd14c
izzycrouch <belle.crouch00@gmail.com>
2025-12-18 16:14:41 +0000

    Feat: creat fact_purchase_order df and text.

--- clean_layer/star_schema_tables/fact_purchase_order.py
+++ clean_layer/star_schema_tables/fact_purchase_order.py
@@ -16,11 +16,16 @@ def create_fact_purchase_order(dim_date_df: pd.DataFrame,
     df = pd.merge(df, dim_currency_df, on='currency_id')
     df = pd.merge(df, dim_staff_df, on='staff_id')
     df = pd.merge(df, dim_counterparty_df, on='counterparty_id')
-    df = pd.merge(df, dim_location_df, left_on='agreed_delivery_location_id', right_on='location_id', how='left').drop('location_id', axis=1)
+    df = pd.merge(df, dim_location_df['location_id'], left_on='agreed_delivery_location_id', right_on='location_id', how='left').drop('location_id', axis=1)
     
-    df.insert(loc=2, column='created_date', value=df['created_at'].dt.date)
+    df.insert(loc=2, column='created_date', value=pd.to_datetime(df['created_at'].dt.date))
     df.insert(loc=3, column='created_time', value=df['created_at'].dt.time)
-    df = pd.merge(df, dim_date_df, left_on='created_date', right_on='location_id', how='left').drop('location_id', axis=1)
+    df['created_time'] = pd.to_datetime(df['created_time'], format='%H:%M:%S.%f')
+    df = pd.merge(df, dim_date_df['date_id'], left_on='created_date', right_on='date_id', how='left').drop(['date_id', 'created_at'], axis=1)
 
-    print(df.columns.values.tolist())
+    df.insert(loc=4, column='last_updated_date', value=pd.to_datetime(df['last_updated'].dt.date))
+    df.insert(loc=5, column='last_updated_time', value=df['last_updated'].dt.time)
+    df['last_updated_time'] = pd.to_datetime(df['last_updated_time'], format='%H:%M:%S.%f')
+    df = pd.merge(df, dim_date_df['date_id'], left_on='last_updated_date', right_on='date_id', how='left').drop(['date_id', 'last_updated'], axis=1)
+    
     return df
--- tests/clean_layer_tests/star_schema_tests/test_fact_purchase_order.py
+++ tests/clean_layer_tests/star_schema_tests/test_fact_purchase_order.py
@@ -67,3 +67,76 @@ class TestFactPurchaseOrder:
         output_columns = result.columns.values.tolist()
         
         assert expected_columns == output_columns
+    
+    def test_create_fact_purchase_order_returns_correct_column_datatyped(self):
+        dim_date_df = create_dim_date(start='2025-12-14', end='2025-12-21')
+        dim_currency_df = pd.DataFrame([{'currency_id': 1}])
+        dim_staff_df = pd.DataFrame([{'staff_id' : 1}])
+        dim_counterparty_df = pd.DataFrame([{'counterparty_id': 1}])
+        dim_location_df = pd.DataFrame([{'location_id': 1}])
+        purchase_order_df = pd.DataFrame([{'purchase_order_id': 1,
+                                            'created_at': datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+                                            'last_updated': datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+                                            'staff_id': 1,
+                                            'counterparty_id': 1,
+                                            'item_code': 'ZDOI5EA',
+                                            'item_quantity': 1,
+                                            'item_unit_price': 5.99,
+                                            'currency_id': 1,
+                                            'agreed_delivery_date': datetime.fromisoformat('2025-12-20'),
+                                            'agreed_payment_date': datetime.fromisoformat('2025-12-17'),
+                                            'agreed_delivery_location_id': 1}])
+
+        result = create_fact_purchase_order(dim_date_df, dim_currency_df, dim_staff_df, dim_counterparty_df, dim_location_df, purchase_order_df)
+        
+        assert result['purchase_record_id'].dtype == int 
+        assert result['purchase_order_id'].dtype == int
+        assert str(result['created_date'].dtype) == 'datetime64[ns]'
+        assert str(result['created_time'].dtype) == 'datetime64[ns]'
+        assert str(result['last_updated_date'].dtype) == 'datetime64[ns]'
+        assert result['last_updated_time'].dtype == 'datetime64[ns]'
+        assert result['staff_id'].dtype == int
+        assert result['counterparty_id'].dtype == int
+        assert result['item_code'].dtype == object
+        assert result['item_quantity'].dtype == int
+        assert result['item_unit_price'].dtype == float
+        assert result['currency_id'].dtype == int
+        assert str(result['agreed_delivery_date'].dtype) == 'datetime64[ns]'
+        assert str(result['agreed_payment_date'].dtype) == 'datetime64[ns]'
+        assert result['agreed_delivery_location_id'].dtype == int
+    
+
+    def test_create_fact_purchase_keeps_history_of_data(self):
+        dim_date_df = create_dim_date(start='2025-12-14', end='2025-12-21')
+        dim_currency_df = pd.DataFrame([{'currency_id': 1}])
+        dim_staff_df = pd.DataFrame([{'staff_id' : 1}])
+        dim_counterparty_df = pd.DataFrame([{'counterparty_id': 1}])
+        dim_location_df = pd.DataFrame([{'location_id': 1}])
+        purchase_order_df = pd.DataFrame([{'purchase_order_id': 1,
+                                            'created_at': datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+                                            'last_updated': datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+                                            'staff_id': 1,
+                                            'counterparty_id': 1,
+                                            'item_code': 'ZDOI5EA',
+                                            'item_quantity': 1,
+                                            'item_unit_price': 5.99,
+                                            'currency_id': 1,
+                                            'agreed_delivery_date': datetime.fromisoformat('2025-12-20'),
+                                            'agreed_payment_date': datetime.fromisoformat('2025-12-17'),
+                                            'agreed_delivery_location_id': 1},
+                                            {'purchase_order_id': 1,
+                                            'created_at': datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+                                            'last_updated': datetime.fromisoformat('2025-12-16 15:51:20.825099'),
+                                            'staff_id': 1,
+                                            'counterparty_id': 1,
+                                            'item_code': 'ZDOI5EA',
+                                            'item_quantity': 2,
+                                            'item_unit_price': 5.99,
+                                            'currency_id': 1,
+                                            'agreed_delivery_date': datetime.fromisoformat('2025-12-20'),
+                                            'agreed_payment_date': datetime.fromisoformat('2025-12-17'),
+                                            'agreed_delivery_location_id': 1}])
+
+        result = create_fact_purchase_order(dim_date_df, dim_currency_df, dim_staff_df, dim_counterparty_df, dim_location_df, purchase_order_df)
+        
+        assert result.shape == (2, 15)
