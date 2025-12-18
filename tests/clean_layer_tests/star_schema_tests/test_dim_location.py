f60399e5f2082e2347d24743812c090b78270bb6
Shyqwq5 <shyqwq5@gmail.com>
2025-12-18 12:42:19 +0000

    add location func and test

---
+++ tests/clean_layer_tests/star_schema_tests/test_dim_location.py
@@ -0,0 +1,80 @@
+from clean_layer.star_schema_tables.dim_location import create_dim_location
+import pandas as pd
+from datetime import datetime
+
+
+class TestDimLocation:
+    def test_returns_dataframe(self):
+        test_data = [{
+            "address_id": 1,
+            "address_line_1": "123 Baker Street",
+            "address_line_2": "Apt 4",
+            "district": "Central",
+            "city": "London",
+            "postal_code": "NW1 6XE",
+            "country": "UK",
+            "phone": "0123456789",
+            "created_at": datetime.fromisoformat("2022-11-03 14:20:49.962000"),
+            "last_updated": datetime.fromisoformat("2022-11-03 14:20:49.962000"),
+        }]
+        cleaned = pd.DataFrame(test_data)
+
+        df = create_dim_location(cleaned)
+
+        assert isinstance(df, pd.DataFrame)
+
+    def test_correct_columns(self):
+        test_data = [{
+            "address_id": 1,
+            "address_line_1": "123 Baker Street",
+            "address_line_2": "Apt 4",
+            "district": "Central",
+            "city": "London",
+            "postal_code": "NW1 6XE",
+            "country": "UK",
+            "phone": "0123456789",
+            "created_at": datetime.fromisoformat("2022-11-03 14:20:49.962000"),
+            "last_updated": datetime.fromisoformat("2022-11-03 14:20:49.962000"),
+        }]
+        cleaned = pd.DataFrame(test_data)
+
+        df = create_dim_location(cleaned)
+
+        expected_cols = [
+            "location_id",
+            "address_line_1",
+            "address_line_2",
+            "district",
+            "city",
+            "postal_code",
+            "country",
+            "phone",
+        ]
+
+        assert list(df.columns) == expected_cols
+
+    def test_correct_data_types(self):
+        test_data = [{
+            "address_id": 1,
+            "address_line_1": "123 Baker Street",
+            "address_line_2": "Apt 4",
+            "district": "Central",
+            "city": "London",
+            "postal_code": "NW1 6XE",
+            "country": "UK",
+            "phone": "0123456789",
+            "created_at": datetime.fromisoformat("2022-11-03 14:20:49.962000"),
+            "last_updated": datetime.fromisoformat("2022-11-03 14:20:49.962000"),
+        }]
+        cleaned = pd.DataFrame(test_data)
+
+        df = create_dim_location(cleaned)
+
+        assert df["location_id"].dtype == "int64"
+        assert df["address_line_1"].dtype == object
+        assert df["address_line_2"].dtype == object
+        assert df["district"].dtype == object
+        assert df["city"].dtype == object
+        assert df["postal_code"].dtype == object
+        assert df["country"].dtype == object
+        assert df["phone"].dtype == object
