f19c18cea020923e7ea04092e0e881e2e7d2ecda
Adina Keller <adinakeller15@gmail.com>
2025-12-18 11:46:13 +0000

    fix: simplify and fix tests

--- clean_layer/star_schema_tables/dim_design.py
+++ clean_layer/star_schema_tables/dim_design.py
@@ -1,5 +1,3 @@
-from clean_layer.clean_func.clean_design import clean_design
-
 def create_dim_design(cleaned_design):
     dim_design = (
         cleaned_design[
@@ -12,5 +10,3 @@ def create_dim_design(cleaned_design):
         ]
     )
     return dim_design
-
-#create_dim_design(clean_design('design/year=2025/month=12/day=15/batch_20251215T135936Z.parquet'))
