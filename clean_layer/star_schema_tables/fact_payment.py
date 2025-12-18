6eedc05dd4fea6fab67231004059969685a17bd1
Adina Keller <adinakeller15@gmail.com>
2025-12-18 15:51:57 +0000

    feat: create and test fact_payment table

---
+++ clean_layer/star_schema_tables/fact_payment.py
@@ -0,0 +1,39 @@
+def create_fact_payment(payment, dim_payment_type, dim_transaction, dim_counterparty, dim_currency, dim_date):
+    df = (
+        payment
+        .merge(dim_payment_type, on='payment_type_id')
+        .merge(dim_transaction, on='transaction_id')
+        .merge(dim_counterparty, on='counterparty_id')
+        .merge(dim_currency, on='currency_id')
+        .merge(dim_date, how='left', left_on='payment_date', right_on='date_id')
+    )
+
+    df.insert(loc=0, column='payment_record_id', value=df.index + 1)
+
+    df.insert(loc=2, column='created_date', value=df['created_at'].dt.date)
+    df.insert(loc=3, column='created_time', value=df['created_at'].dt.time)
+    df.insert(loc=4, column='last_updated_date', value=df['last_updated'].dt.date)
+    df.insert(loc=5, column='last_updated_time', value=df['last_updated'].dt.time)
+
+    df = (df.merge(dim_date, left_on='created_date', right_on='date_id', how='left'))
+
+    fact_payment = (
+        df[
+            [
+                "payment_record_id",
+                "payment_id",
+                "created_date",
+                "created_time",
+                "last_updated_date",
+                "last_updated_time",
+                "transaction_id",
+                "counterparty_id",
+                "payment_amount",
+                "currency_id",
+                "payment_type_id",
+                "paid",
+                "payment_date"
+            ]
+        ]
+    )
+    return fact_payment
---
+++ tests/clean_layer_tests/star_schema_tests/test_fact_payment.py
@@ -0,0 +1,110 @@
+from clean_layer.star_schema_tables.fact_payment import create_fact_payment
+import pandas as pd
+from datetime import datetime 
+
+class TestFactPayment:
+    def test_returns_dataframe(self):
+
+        payment = pd.DataFrame([{
+            'payment_id' : 1,
+            'created_at' : datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+            'last_updated' : datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+            'transaction_id' : 2,
+            'counterparty_id' : 3,
+            'payment_amount': 2.50,
+            'currency_id': 4,
+            'payment_type_id': 5,
+            'paid': True,
+            'payment_date': datetime.fromisoformat('2025-12-15'),
+            'company_ac_number' : 12345678,
+            'counterparty_ac_number' : 87654321
+            }])
+        dim_payment_type = pd.DataFrame([{"payment_type_id": 1}])
+        dim_transaction = pd.DataFrame([{'transaction_id' : 1}])
+        dim_counterparty = pd.DataFrame([{'counterparty_id': 1}])
+        dim_currency = pd.DataFrame([{'currency_id': 1}])
+        dim_date = pd.DataFrame([{'date_id': 1}])
+
+        df = create_fact_payment(payment, dim_payment_type, dim_transaction, dim_counterparty, dim_currency, dim_date)
+
+        assert isinstance(df, pd.DataFrame)
+
+    def test_correct_columns(self):
+
+        payment = pd.DataFrame([{
+            'payment_id' : 1,
+            'created_at' : datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+            'last_updated' : datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+            'transaction_id' : 2,
+            'counterparty_id' : 3,
+            'payment_amount': 2.50,
+            'currency_id': 4,
+            'payment_type_id': 5,
+            'paid': True,
+            'payment_date': datetime.fromisoformat('2025-12-15'),
+            'company_ac_number' : 12345678,
+            'counterparty_ac_number' : 87654321
+            }])
+        dim_payment_type = pd.DataFrame([{"payment_type_id": 1}])
+        dim_transaction = pd.DataFrame([{'transaction_id' : 1}])
+        dim_counterparty = pd.DataFrame([{'counterparty_id': 1}])
+        dim_currency = pd.DataFrame([{'currency_id': 1}])
+        dim_date = pd.DataFrame([{'date_id': 1}])
+
+        df = create_fact_payment(payment, dim_payment_type, dim_transaction, dim_counterparty, dim_currency, dim_date)
+
+        expected_cols = [
+                "payment_record_id",
+                "payment_id",
+                "created_date",
+                "created_time",
+                "last_updated_date",
+                "last_updated_time",
+                "transaction_id",
+                "counterparty_id",
+                "payment_amount",
+                "currency_id",
+                "payment_type_id",
+                "paid",
+                "payment_date"
+            ]
+        
+        assert (expected_cols == df.columns).any()
+
+    def test_correct_data_types(self):
+
+        payment = pd.DataFrame([{
+            'payment_id' : 1,
+            'created_at' : datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+            'last_updated' : datetime.fromisoformat('2025-12-15 15:51:20.825099'),
+            'transaction_id' : 2,
+            'counterparty_id' : 3,
+            'payment_amount': 2.50,
+            'currency_id': 4,
+            'payment_type_id': 5,
+            'paid': True,
+            'payment_date': datetime.fromisoformat('2025-12-15'),
+            'company_ac_number' : 12345678,
+            'counterparty_ac_number' : 87654321
+            }])
+        dim_payment_type = pd.DataFrame([{"payment_type_id": 1}])
+        dim_transaction = pd.DataFrame([{'transaction_id' : 1}])
+        dim_counterparty = pd.DataFrame([{'counterparty_id': 1}])
+        dim_currency = pd.DataFrame([{'currency_id': 1}])
+        dim_date = pd.DataFrame([{'date_id': 1}])
+
+        df = create_fact_payment(payment, dim_payment_type, dim_transaction, dim_counterparty, dim_currency, dim_date)
+        print(df.dtypes)
+        assert df["payment_record_id"].dtype == int
+        assert df["payment_id"].dtype == int
+        assert df["created_date"].dtype == object
+        assert df["created_time"].dtype == object
+        assert df["last_updated_date"].dtype == object
+        assert df["last_updated_time"].dtype == object
+        assert df["transaction_id"].dtype == int
+        assert df["counterparty_id"].dtype == int
+        assert df["payment_amount"].dtype == float
+        assert df["currency_id"].dtype == int
+        assert df["payment_type_id"].dtype == int
+        assert df["paid"].dtype == bool
+        assert df["payment_date"].dtype == 'datetime64[ns]'
