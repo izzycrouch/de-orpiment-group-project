645188bb1eae5ca9fcc3b3d96f2bf60158a0da56
izzycrouch <belle.crouch00@gmail.com>
2026-01-07 14:09:49 +0000

    Feat: iam permissions for state machine.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -116,23 +116,42 @@ resource "aws_iam_role_policy_attachment" "attach_cloud_watch_to_transform" {
 
 
 
-resource "aws_iam_role" "iam_for_sfn" {
-  name               = "state_machine_role"
-  assume_role_policy = data.aws_iam_policy_document.state_machine_assume_role.json
-  tags = {
-    tag-key = "tag-value"
-  }
+resource "aws_iam_role" "step_functions_role" {
+    name = "step_functions_role_poc_sf"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+            Service = "states.amazonaws.com"
+            }
+        }
+        ]
+    })
 }
 
-data "aws_iam_policy_document" "state_machine_assume_role" {
-  statement {
-    actions = [
-      "lambda:InvokeFunction"
-    ]
-    resources = [
-      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${resource.aws_lambda_function.extract_raw_data_function.function_name}}",
-      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${resource.aws_lambda_function.transform_data_function.function_name}}"
-    ]
-  }
+
+
+data "aws_iam_policy_document" "lambda_access_policy" {
+    statement {
+        actions = [
+        "lambda:*"
+        ]
+        resources = ["*"]
+    }
 }
 
+
+resource "aws_iam_policy" "step_functions_policy_lambda" {
+    name   = "step_functions_policy_lambda_policy_all_poc_sf"
+    policy = data.aws_iam_policy_document.lambda_access_policy.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "step_functions_to_lambda" {
+    role       = aws_iam_role.step_functions_role.name
+    policy_arn = aws_iam_policy.step_functions_policy_lambda.arn
+}
