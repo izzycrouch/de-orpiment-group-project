adf3a2a05080acb9278d273a88c690d2921ebfb9
Shyqwq5 <shyqwq5@gmail.com>
2026-01-08 11:51:50 +0000

    chore: update extract script configuration

--- run_extract.py
+++ run_extract.py
@@ -1,7 +1,8 @@
 import os
 os.environ["S3_RAW_BUCKET_NAME"] = "totesys-raw-data-aci"
 os.environ["S3_PROCESSED_BUCKET_NAME"]= "totesys-transformed-data-aci-2"
+from extract_layer.extract_lambda import lambda_handler
 from clean_layer.clean import lambda_handler
 
 
-lambda_handler('a','b')
+lambda_handler(['payment/year=2026/month=1/day=7/batch_20260107T121905Z.parquet'],'b')
