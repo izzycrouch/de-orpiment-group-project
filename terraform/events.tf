b855df874b68ed425e6f5917eb77bf8a9608504b
Shyqwq5 <shyqwq5@gmail.com>
2026-01-08 11:51:43 +0000

    chore: update EventBridge rules configuration

--- terraform/events.tf
+++ terraform/events.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_event_rule" "15_min_scheduler" {
+resource "aws_cloudwatch_event_rule" "scheduler" {
   name                = "trigger-state-machine"
   description         = "Trigger State Machine Every 15 Mins"
   schedule_expression = "rate(15 minutes)"
@@ -8,5 +8,5 @@ resource "aws_cloudwatch_event_target" "state_machine" {
   rule      = aws_cloudwatch_event_rule.scheduler.name
   target_id = "trigger-state-machine"
   arn       = aws_sfn_state_machine.sfn_state_machine.arn
-  role_arn  = aws_iam_role.iam_for_sfn.arn
+  role_arn  = aws_iam_role.iam_for_eventbridge.arn
 }
